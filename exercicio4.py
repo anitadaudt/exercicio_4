91f1ba86b553b0c46520a63fb97686bc941122bf
anitadaudt <anitadaudt@gmail.com>
2025-02-27 18:23:25 -0300

    Add files via upload

---
+++ exercicio4.py
@@ -0,0 +1,21 @@
+"""
+Exercício 4
+Autor: Anita Daudt
+Data: 27/02/2025
+Versão 0.0.1
+"""
+
+# Alocação de memória: 
+
+nome: ""
+
+# Entrada de dados: 
+
+nome = input("Qual é o seu nome? ")
+
+# Processamento de dados:
+
+# Saída de dados: 
+
+print(f"Oi, {nome}!")
+
